cc9c4aa6f2d4f06cc6496adc1d89bbc676b0fcf7
lamden000 <lamden150@gmail.com>
2026-08-07 14:29:14 +0700

    Add an empty project

---
+++ TinyGameEngine.cpp
@@ -0,0 +1,12 @@
+﻿// TinyGameEngine.cpp : Defines the entry point for the application.
+//
+
+#include "TinyGameEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
